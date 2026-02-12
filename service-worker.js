ca709f6b513d9e96138cd5bc3091a7f41ff401de
Yeshua3x <rochaeterna12@outlook.com>
2026-02-12 00:16:30 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+{
+  "name": "PF Concursos",
+  "short_name": "Concursos",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#007BFF",
+  "theme_color": "#f4f4f9",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "icon-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icon-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
